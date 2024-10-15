214ab0b0750be2b407903952820eb926bcb176a7
Mohid Kazi <mohid-kazi@Mohid-MacBook-Air.local>
2024-10-15 21:51:45 +0530

    chore(code): remove files and optimize directory structure

--- src/Person.ts
+++ src/Person.ts
@@ -1,4 +1,37 @@
-export default class Person {
+export class Base {
+  private input: string;
+  private N: number;
+
+  private output: string | number;
+
+  constructor() {}
+
+  public set _input(input: string) {
+    this.input = input;
+    this.N = input.length - 1;
+  }
+
+  public get _output(): string | number {
+    return this.output;
+  }
+
+  public process() {
+    for (let index = 0; index <= this.N; index++) {
+      console.log(this.input[index]);
+    }
+  }
+
+  public log() {
+    console.log(this.output);
+  }
+}
+
+const base: Base = new Base();
+base._input = '';
+base.process();
+base.log();
+
+export class Person {
   public name;
   public designation;
 
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,19 @@
-// import Person from './Person';
-import testTwo from './testTwo';
-// import testOne from './testOne';
+import * as readline from 'node:readline/promises';
+import { stdin, stdout, exit } from 'node:process';
+import ATMMachine from './Interview/ATMMachine';
 
-// const jim = new Person('Mohid', 'Developer');
-// console.log(jim.introduce());
-// console.log(jim.name);
+const amount = 2700;
+const amtMachine: ATMMachine = new ATMMachine(amount);
 
-/**
- * test one -
- * for input [1, 2, 1, 4, 5, 3, 4, 5, 6, 7, 8, 9, 10, 11]
- * output should be in set of 2 & 3 where the values should not repeat
- * output - [
- * [1, 2],
- * [4, 5, 3],
- * [6, 7],
- * [8, 9, 10],
- * [11]
- * ]
- */
+async function withdraw() {
+  const rl = readline.createInterface({ input: stdin, output: stdout });
+  const input = await rl.question('Please provide the amount you want => ');
+  input.split('|').map((amount) => {
+    amtMachine.amount = Number(amount);
+    amtMachine.process();
+  });
+  // rl.write('Please collect your note\n');
+  exit();
+}
 
-// console.log(testOne([1, 2, 1, 4, 5, 3, 4, 5, 6, 7, 8, 9, 10, 11]));
-
-/**
- * https://codeinterview.io/SDEOUMXQTF
- * 1. For input "h[ello](!)" the output was incorrect. The correct output is 1 2
- * 2. For input "one(bracket)" the output was incorrect. The correct output is 1 1
- * 3. For input "coder(b)(y)(t)([e))" the output was incorrect. The correct output is 0
- * 4. For input "()code[rb]yte() yes()[ss][[" the output was incorrect. The correct output is 0
- * 5. For input "dogs and cats([(1)])" the output was incorrect. The correct output is 1 3
- * 6. For input "01()01()01()[10]" the output was incorrect. The correct output is 1 4
- * 7. For input "the co[l][]or re(d))()(()" the output was incorrect. The correct output is 0
- * 8. For input "le[tter(s) gal](o)(r)((e])" the output was incorrect. The correct output is 0
- * 9. For input "thr[e](e) let(t)ers" the output was incorrect. The correct output is 1 3
- * 10. For input "twice thri(c)(e)()()[[[[dash]]]]" the output was incorrect. The correct output is 1 8
- */
-
-// console.log(testTwo('h[ello](!)'), 'output: 1 2');
-// console.log(testTwo('one(bracket)'), 'output: 1 1');
-// console.log(testTwo('coder(b)(y)(t)([e))'), 'output: 0');
-// console.log(testTwo('()code[rb]yte() yes()[ss][['), 'output: 0');
-console.log(testTwo('dogs and cats([(1)])'), 'output: 1 3');
-// console.log(testTwo('01()01()01()[10]'), 'output: 1 4');
-// console.log(testTwo('he co[l][]or re(d))()(()'), 'output: 0');
-
-//317 vashi plaza
+withdraw();
